b1ab5e2caea96de1025f7f3ad476df517a8c11f5
Yrikk7 <urikk7@gmail.com>
2026-05-09 23:26:37 +0300

    fix vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+
 const express = require("express")
 const path = require("path")
 const connectDB = require("../db")
@@ -6,20 +7,15 @@ const authRouter = require("../authRouter")
 
 const app = express()
 
-// подключение БД
 connectDB().catch(console.error)
 
-// EJS
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "../views"))
 
-// STATIC
 app.use(express.static(path.join(__dirname, "../public")))
 
-// JSON
 app.use(express.json())
 
-// ROUTES
 app.use("/auth", authRouter)
 
 module.exports = app
